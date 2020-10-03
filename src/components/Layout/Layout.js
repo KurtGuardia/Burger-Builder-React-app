74f9ac0891427b5598b4a39153ccc81176591719
Kurt Guardia <kurtguardia@gmail.com>
2020-10-03 13:08:43 +0200

    set the functionallity to SideDrawer component and created a functional Hamburger button

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
 import classes from "./Layout.module.css";
-import Toolbar from '../../components/Navigation/Toolbar/Toolbar'
-import SideDrawer from '../Navigation/SidreDrawer/SideDrawer';
+import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const layout = (props) => (
-  <Aux>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>{props.children}</main>
-  </Aux>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: false
+  }
 
-export default layout;
+  sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer: false})
+  }
+
+  sideDrawerOpenHandler = () => {
+    this.setState({showSideDrawer: true})
+  } 
+
+  render() {
+    return(
+      <Aux>
+        <Toolbar open={this.sideDrawerOpenHandler} />
+        <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
+
+export default Layout;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,17 +2,22 @@ import React from "react";
 import classes from "./Toolbar.module.css";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
+import Ham from '../Ham/Ham'
 
 const toolbar = (props) => {
   return (
     <header className={classes.Toolbar}>
-      <div>MENU</div>
+
+      <Ham clicked={props.open} />
+
       <div className={classes.Logo}>
         <Logo />
       </div>
+
       <nav className={classes.DesktopOnly}>
         <NavigationItems />
       </nav>
+
     </header>
   );
 };
