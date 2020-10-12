7a2a1e925c4e69670cf4c39c6d663d92b6fe44bc
Kurt Guardia <kurtguardia@gmail.com>
2020-10-12 09:57:39 +0200

    Created the Orders page or component and added to the Navigation items

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import NavigationItem from './NavigationItem/NavigationItem'
-import classes from './NavigationItems.module.css'
+import React from "react";
+import NavigationItem from "./NavigationItem/NavigationItem";
+import classes from "./NavigationItems.module.css";
 
 const navigationItems = (props) => {
-    return (
-        <ul className={classes.NavigationItems}>
-            <NavigationItem link='/' active>Burger Builder</NavigationItem>
-            <NavigationItem link='/'>Checkout</NavigationItem>
-            
-        </ul>)
-}
+  return (
+    <ul className={classes.NavigationItems}>
+      <NavigationItem exact link="/">
+        Burger Builder
+      </NavigationItem>
+      <NavigationItem link="/orders">Orders</NavigationItem>
+    </ul>
+  );
+};
 
-export default navigationItems
+export default navigationItems;
