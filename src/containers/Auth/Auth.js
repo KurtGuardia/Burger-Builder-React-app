392edd1616478436bdf5b7f1a57183e86e9db818
Kurt Guardia <kurtguardia@gmail.com>
2020-10-24 13:49:50 +0200

    Added Firebase backend functioanllity to SignUp

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,106 +2,127 @@ import React, { Component } from "react";
 import Input from "../../components/UI/Input/Input";
 import Button from "../../components/UI/Button/Button";
 import classes from "./Auth.module.css";
+import * as actions from "../../store/actions/index";
+import { connect } from "react-redux";
 
-export default class Auth extends Component {
-  state = {
-    controls: {
-      email: {
-        elementType: "input",
-        elementConfig: {
-          type: "email",
-          placeholder: "Mail Address",
-        },
-        value: "",
-        validation: {
-          required: true,
-          isEmail: true,
-        },
-        valid: false,
-        touched: false,
-      },
-      password: {
-        elementType: "input",
-        elementConfig: {
-          type: "password",
-          placeholder: "Password",
-        },
-        value: "",
-        validation: {
-          required: true,
-          minLength: 6,
-        },
-        valid: false,
-        touched: false,
-      },
-    },
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onAuth: (email, password) => dispatch(actions.auth(email, password)),
   };
-
-  checkValidity(value, rules) {
-    let isValid = true;
-
-    if (rules.required) {
-      isValid = value.trim() !== "" && isValid;
-    }
-
-    if (rules.minLength) {
-      isValid = value.length >= rules.minLength && isValid;
-    }
-
-    if (rules.isEmail) {
-      const pattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-      isValid = pattern.test(value) && isValid;
-    }
-    return isValid;
-  }
-
-  inputChangedHandler = (event, controlName) => {
-    const updatedControls = {
-      ...this.state.controls,
-      [controlName]: {
-        ...this.state.controls[controlName],
-        value: event.target.value,
-        valid: this.checkValidity(
-          event.target.value,
-          this.state.controls[controlName].validation
-        ),
-        touched: true,
+};
+export default connect(
+  null,
+  mapDispatchToProps
+)(
+  class Auth extends Component {
+    state = {
+      controls: {
+        email: {
+          elementType: "input",
+          elementConfig: {
+            type: "email",
+            placeholder: "Mail Address",
+          },
+          value: "",
+          validation: {
+            required: true,
+            isEmail: true,
+          },
+          valid: false,
+          touched: false,
+        },
+        password: {
+          elementType: "input",
+          elementConfig: {
+            type: "password",
+            placeholder: "Password",
+          },
+          value: "",
+          validation: {
+            required: true,
+            minLength: 6,
+          },
+          valid: false,
+          touched: false,
+        },
       },
     };
 
-    this.setState({ controls: updatedControls });
-  };
+    checkValidity(value, rules) {
+      let isValid = true;
 
-  render() {
-    const formElementsArray = [];
-    for (let key in this.state.controls) {
-      formElementsArray.push({
-        id: key,
-        config: this.state.controls[key],
-      });
+      if (rules.required) {
+        isValid = value.trim() !== "" && isValid;
+      }
+
+      if (rules.minLength) {
+        isValid = value.length >= rules.minLength && isValid;
+      }
+
+      if (rules.isEmail) {
+        const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+        isValid = pattern.test(value) && isValid;
+      }
+      return isValid;
     }
 
-    const form = formElementsArray.map((formElement) => (
-      <Input
-        key={formElement.id}
-        elementType={formElement.config.elementType}
-        elementConfig={formElement.config.elementConfig}
-        value={formElement.config.value}
-        changed={(event) => this.inputChangedHandler(event, formElement.id)}
-        invalid={!formElement.config.valid}
-        shouldValidate={formElement.config.validation}
-        touched={formElement.config.touched}
-        valueType={formElement.id}
-      />
-    ));
+    inputChangedHandler = (event, controlName) => {
+      const updatedControls = {
+        ...this.state.controls,
+        [controlName]: {
+          ...this.state.controls[controlName],
+          value: event.target.value,
+          valid: this.checkValidity(
+            event.target.value,
+            this.state.controls[controlName].validation
+          ),
+          touched: true,
+        },
+      };
 
-    return (
-      <div className={classes.Auth}>
-        <form>
-          {form}
-          <Button btnType="Success">SUBMIT</Button>
-        </form>
-      </div>
-    );
+      this.setState({ controls: updatedControls });
+    };
+
+    submitHandler = (event) => {
+      event.preventDefault();
+
+      this.props.onAuth(
+        this.state.controls.email.value,
+        this.state.controls.password.value
+      );
+    };
+
+    render() {
+      const formElementsArray = [];
+      for (let key in this.state.controls) {
+        formElementsArray.push({
+          id: key,
+          config: this.state.controls[key],
+        });
+      }
+
+      const form = formElementsArray.map((formElement) => (
+        <Input
+          key={formElement.id}
+          elementType={formElement.config.elementType}
+          elementConfig={formElement.config.elementConfig}
+          value={formElement.config.value}
+          changed={(event) => this.inputChangedHandler(event, formElement.id)}
+          invalid={!formElement.config.valid}
+          shouldValidate={formElement.config.validation}
+          touched={formElement.config.touched}
+          valueType={formElement.id}
+        />
+      ));
+
+      return (
+        <div className={classes.Auth}>
+          <form onSubmit={this.submitHandler}>
+            {form}
+            <Button btnType="Success">SUBMIT</Button>
+          </form>
+        </div>
+      );
+    }
   }
-}
+);
